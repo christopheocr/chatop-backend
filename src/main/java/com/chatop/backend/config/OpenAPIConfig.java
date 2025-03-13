07202f5bda60ace1c6c2cb57fb6f2cf8d250a288
Christophe <christophe.ocr@outlook.fr>
2025-03-13 21:09:23 +0100

    Modification OpenAPIDefinition

--- src/main/java/com/chatop/backend/config/OpenAPIConfig.java
+++ src/main/java/com/chatop/backend/config/OpenAPIConfig.java
@@ -1,13 +1,25 @@
 package com.chatop.backend.config;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
-import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
 
 @OpenAPIDefinition(
-        info = @Info(title = "ChatOp", version = "1.0")
+        info = @Info(title = "ChatOp",
+                version = "1.0",
+                description = "API de gestion de l'application ChatOp",
+                contact = @Contact(
+                        name = "Christophe",
+                        url = "https://github.com/christopheocr"
+                )
+        ),
+        externalDocs = @ExternalDocumentation(
+                description = "Code source du projet sur GitHub",
+                url = "https://github.com/christopheocr/chatop-backend"
+        )
 )
 @SecurityScheme(
         name = "BearerAuth",
